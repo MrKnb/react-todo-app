1f1025269fd1167dd8432f3c35e690cb1ad095e9
MrKnb <mrknabe2@gmail.com>
2020-07-22 17:54:25 +0200

    static back arrow

---
+++ src/components/header/backArrow.js
@@ -0,0 +1,9 @@
+/*function backToCategory() {
+
+if (backToCategory) {
+  backToCategory.addEventListener('click', () => {
+    window.location.href = 'http://127.0.0.1:5500/index.html'
+  })
+}}
+
+export default backToCategory;*/
