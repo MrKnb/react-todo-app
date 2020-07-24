00ba037ea8906bacf3d8efbc0fdea728ea1e48da
Erick Jansen <janseniter@gmail.com>
2020-07-25 01:33:25 +0200

    add localStorage

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -1,40 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Category from '../category';
 import AddNew from '../addButton';
 import Styles from './index.module.css';
 import Form from '../form';
 
-const data = [
-  {
-    id: 30,
-    title: 'category1',
-    todos: [],
-  },
-  {
-    id: 32,
-    title: 'category2',
-    todos: [],
-  },
-  {
-    id: 34,
-    title: 'category3',
-    todos: [],
-  },
-];
-
 function Categories() {
-  const [categories, setCategories] = useState(data);
+  const [categories, setCategories] = useState([]);
   const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    setCategories(JSON.parse(window.localStorage.getItem('Categories')));
+  }, []);
 
   const displayForm = () => setShowForm(true);
 
   return (
     <div className="categories">
       <h1 className={Styles.categoryHeading}>Categories</h1>
-      {categories.map((category) => (
-        <Category key={category.id} id={category.id} title={category.title} />
-      ))}
+      {categories ? (
+        categories.map((category) => (
+          <Category key={category.id} id={category.id} title={category.title} />
+        ))
+      ) : (
+        <p>no categories</p>
+      )}
 
       <div className={Styles.addButton}>
         {showForm ? <Form /> : null}
