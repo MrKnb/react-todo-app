06d9d54622f7d5996f7c290f7a17a1e5e8a610dc
Erick Jansen <janseniter@gmail.com>
2020-07-24 14:57:57 +0200

    start development of todo deletion

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -9,24 +9,24 @@ const data = [
     id: 12,
     title: 'category1',
     todos: [
-      { title: 'buy beer', isDone: false },
-      { title: 'buy pizza', isDone: false },
+      { id: 1, title: 'buy beer', isDone: false },
+      { id: 2, title: 'buy pizza', isDone: false },
     ],
   },
   {
     id: 23,
     title: 'category2',
     todos: [
-      { title: 'buy beer', isDone: false },
-      { title: 'buy pizza', isDone: false },
+      { id: 3, title: 'buy beer', isDone: false },
+      { id: 4, title: 'buy pizza', isDone: false },
     ],
   },
   {
     id: 53,
     title: 'category3',
     todos: [
-      { title: 'buy beer', isDone: false },
-      { title: 'buy pizza', isDone: false },
+      { id: 5, title: 'buy beer', isDone: false },
+      { id: 6, title: 'buy pizza', isDone: false },
     ],
   },
 ];
@@ -36,13 +36,28 @@ function Category({ match }) {
   const matchedCategory = categories.filter(
     (category) => category.id == match.params.id
   )[0];
+
+  const deleteTodo = (todoID) => {
+    const newItems = matchedCategory.todos.filter(({ id }) => id !== todoID);
+    matchedCategory.todos = newItems;
+    // setCategories({ ...categories, matchedCategory });
+    console.log(matchedCategory.todos);
+  };
+
   return (
     <div className="category-container">
       <h1 className={Styles.categoryName}>{matchedCategory.title}</h1>
       <ul>
         {matchedCategory.todos.map((todo, index) => {
           console.log(index);
-          return <Todo key={index} title={todo.title} />;
+          return (
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              delete={deleteTodo}
+              title={todo.title}
+            />
+          );
         })}
       </ul>
       <div className={Styles.addButton}>
