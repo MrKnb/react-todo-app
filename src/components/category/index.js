d40c07f5639034a799d4b177cccc3172366d7102
Erick Jansen <janseniter@gmail.com>
2020-07-26 01:17:22 +0200

    create edit category operatioin

--- src/components/category/index.js
+++ src/components/category/index.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import Styles from './index.module.css';
+import EditForm from '../editForm';
 
 function Category(props) {
+  const [showEditForm, setShowEditForm] = useState(false);
+
+  const ref = React.createRef();
+
+  const toggleForm = () => {
+    setShowEditForm(!showEditForm);
+  };
   return (
-    <li className={Styles.singleCategory}>
-      <div>
-        <Link className={Styles.Link} to={`/category/${props.id}`}>
-          {props.title}
-        </Link>
-      </div>
-      <div className={Styles.icons}>
-        <i className="material-icons">create</i>
-        <i className="material-icons" onClick={() => props.delete(props.id)}>
-          delete
-        </i>
-      </div>
-    </li>
+    <>
+      <li className={Styles.singleCategory}>
+        <div>
+          <Link className={Styles.Link} to={`/category/${props.id}`}>
+            {props.title}
+          </Link>
+        </div>
+        <div className={Styles.icons}>
+          <i className="material-icons" onClick={() => toggleForm()}>
+            create
+          </i>
+          <i className="material-icons" onClick={() => props.delete(props.id)}>
+            delete
+          </i>
+        </div>
+      </li>
+      {showEditForm ? (
+        <EditForm
+          value={props.title}
+          id={props.id}
+          editItem={props.edit}
+          hideForm={setShowEditForm}
+          ref={ref}
+        />
+      ) : null}
+    </>
   );
 }
 
---
+++ src/components/editForm/index.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+import Styles from './index.module.css';
+
+// function EditForm(props) {
+//   const [value, setValue] = useState(props.value);
+
+//   const onSubmission = (event) => {
+//     event.preventDefault();
+//     props.editItem(props.id, value);
+//     props.hideForm();
+//   };
+
+//   return (
+//     <form className={Styles.addItemForm} onSubmit={onSubmission}>
+//       <input
+//         type="text"
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//       />
+//     </form>
+//   );
+// }
+
+const EditForm = React.forwardRef((props, ref) => {
+  const [value, setValue] = useState(props.value);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+
+  const onSubmission = (event) => {
+    event.preventDefault();
+    props.editItem(props.id, value);
+    props.hideForm();
+  };
+
+  return (
+    <form className={Styles.addItemForm} onSubmit={onSubmission}>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        ref={ref}
+      />
+    </form>
+  );
+});
+
+export default EditForm;
