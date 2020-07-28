ef0b10582544755bf18dd0ce248a7ca519d636e4
Erick Jansen <janseniter@gmail.com>
2020-07-28 13:53:10 +0200

    Updates

---
+++ precache-manifest.42672bc12765e00b5e7bf6f1525de6aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5720d4a3e2febaf55edd729d481054",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "fddf0e5b616a123d2b1c",
+    "url": "/react-todo-app/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "9bc4609fdc5e04cb7a7e",
+    "url": "/react-todo-app/static/css/main.ba28909c.chunk.css"
+  },
+  {
+    "revision": "fddf0e5b616a123d2b1c",
+    "url": "/react-todo-app/static/js/2.d42b780d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.d42b780d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc4609fdc5e04cb7a7e",
+    "url": "/react-todo-app/static/js/main.d4f467fe.chunk.js"
+  },
+  {
+    "revision": "1528fb5b7daed31266f4",
+    "url": "/react-todo-app/static/js/runtime-main.1dd0cb46.js"
+  }
+]);
